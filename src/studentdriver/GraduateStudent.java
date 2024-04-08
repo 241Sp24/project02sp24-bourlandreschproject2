cdc9251a8775640d4183e07bea2b08e8e59dc2ef
Crystian81 <Crystian.Resch81@gmail.com>
2024-04-07 22:36:31 -0500

    Fixing errors from pulling
    I am trying to fix the errors caused by pulling + pushing.

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -2,30 +2,28 @@ package studentdriver;
 
 public class GraduateStudent extends StudentFees {
 
-<<<<<<< HEAD
-=======
-public class GraduateStudent extends StudentFees {
     int coursesEnrolled;
     boolean isGraduateAssistant = true;
     String graduateAssistantType;
     double ADDITIONAL_FEES = 654.45;
-    
-    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, 
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled,
             boolean isGraduateStudent, String graduateAssistantType, int coursesEnrolled) {
         super(studentName, studentID, isEnrolled);
         this.isGraduateAssistant = isGraduateStudent;
         this.graduateAssistantType = graduateAssistantType;
         this.coursesEnrolled = coursesEnrolled;
     }
-    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, 
+
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled,
             boolean isGraduateAssistant, int coursesEnrolled) {
         super(studentName, studentID, isEnrolled);
         this.isGraduateAssistant = isGraduateAssistant;
         this.coursesEnrolled = coursesEnrolled;
     }
-    
+
     public boolean isIsGraduateAssistant() {
-        if(!isGraduateAssistant) {
+        if (!isGraduateAssistant) {
             graduateAssistantType = " ";
         }
         return isGraduateAssistant;
@@ -34,26 +32,26 @@ public class GraduateStudent extends StudentFees {
     public int getCoursesEnrolled() {
         return coursesEnrolled;
     }
+
     @Override
     public double getPayableAmount() {
         int coursesEnrolled = getCoursesEnrolled();
         int totalCredits = coursesEnrolled * getCREDITS_PER_COURSE();
         double payableAmount = (totalCredits * getPER_CREDIT_FEE());
-        
-        if(graduateAssistantType.equals("full")) {
+
+        if (graduateAssistantType.equals("full")) {
             payableAmount = 0.0;
-        }
-        else if(graduateAssistantType.equals("half")) {
+        } else if (graduateAssistantType.equals("half")) {
             payableAmount /= 2;
         }
         payableAmount += ADDITIONAL_FEES;
         return payableAmount;
     }
+
     @Override
     public String toString() {
-        return super.toString() + "\nGraduate assistant: " + isIsGraduateAssistant() + 
-                "\nGraduate assistant type: "+ graduateAssistantType + 
-                "\nCourses enrolled: " + getCoursesEnrolled() + "\nPayable amount: " + String.format("%.2f", getPayableAmount());
+        return super.toString() + "\nGraduate assistant: " + isIsGraduateAssistant()
+                + "\nGraduate assistant type: " + graduateAssistantType
+                + "\nCourses enrolled: " + getCoursesEnrolled() + "\nPayable amount: " + String.format("%.2f", getPayableAmount());
     }
->>>>>>> e71f5801472029e4f710321e1bfe7aa129e1684d
 }
--- src/studentdriver/StudentFees.java
+++ src/studentdriver/StudentFees.java
@@ -47,22 +47,10 @@ public abstract class StudentFees {
     }
 
     public abstract double getPayableAmount();
-<<<<<<< HEAD
 
     @Override
     public String toString() {
         return "Student name: " + getStudentName()
                 + "\nStudent id: " + getStudentId() + "\nEnrolled: " + isIsEnrolled();
     }
-
-=======
-        
-   
-    @Override
-    public String toString() {
-        return "Student name: " + getStudentName() + 
-                "\nStudent id: " + getStudentId() + "\nEnrolled: " + isIsEnrolled();
-    }
-    
->>>>>>> e71f5801472029e4f710321e1bfe7aa129e1684d
 }
