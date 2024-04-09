9cf81408dbf53e22f8c2285cf7612069829fd502
Crystian81 <Crystian.Resch81@gmail.com>
2024-04-08 23:48:54 -0500

    I just saved it
    I saved it and repushed

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -62,5 +62,25 @@ public class StudentDriver {
             System.out.println(student.toString());
             System.out.println("");
         }
+        double scholarshipCountUG = 0;
+        double averageStudentFeeUG = 0;
+        int totalCoursesUG = 0;
+
+        int graduateAssistantCountGS = 0;
+        int totalCoursesGS = 0;
+        double averageStudentFeeGS = 0;
+
+        double averageStudentFeeOS = 0;
+
+        System.out.println("**********Undergraduate Students details**********\n Average Students fee:" + averageStudentFeeUG
+                + "Scholarship count: " + scholarshipCountUG + "\nTotal number of courses: " + totalCoursesUG + "\n");
+
+        System.out.println("**********Graduate Students details**********\n" + "Average Students fee: " + averageStudentFeeGS
+                + "\nGraduate Assistantship count: " + graduateAssistantCountGS + "\nTotal number of courses: "
+                + totalCoursesGS + "\n");
+
+        System.out.println("**********Online Students details**********\n"
+                + "Average Students fee: " + averageStudentFeeOS);
     }
+
 }
